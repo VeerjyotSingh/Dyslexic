fa64fbef0d322f9ef776f1f7cc16d0f487858f8e
VeerjyotSingh <103166939+VeerjyotSingh@users.noreply.github.com>
2023-11-06 21:37:21 +0530

    Initial Commit

---
+++ Dyslexic/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Dyslexic
+//
+//  Created by Veerjyot Singh on 06/11/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
